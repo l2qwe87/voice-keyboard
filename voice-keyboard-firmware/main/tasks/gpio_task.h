4754a3c795bd7953ed49c45f3f6cc82c8bd96b33
l2qwe87 <l2qwe87@gmail.com>
2025-11-12 02:45:31 +0300

    Рефакторинг: вынесение задач в отдельные модули

---
+++ voice-keyboard-firmware/main/tasks/gpio_task.h
@@ -0,0 +1,10 @@
+#ifndef GPIO_TASK_H
+#define GPIO_TASK_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// Создать задачу GPIO / Create GPIO task
+void create_gpio_task(void);
+
+#endif // GPIO_TASK_H
